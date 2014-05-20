208120fc6c675c31e807ea749d09235a7759b372
Simon Tarchichi <kartsims@gmail.com>
2014-05-20 16:14:23 +0200

    production environment config

--- inc/config.js
+++ inc/config.js
@@ -1,29 +1,68 @@
-module.exports = {
+// PRODUCTION CONFIG
+if (process.env.SONGZ_ENVIRONMENT=='production'){
 
-  // game parameters
-  game: {
-    // max number of players in a game
-    max_players: 15,
-    // number of songs played in a game
-    nb_songs: 15,
-    // time in seconds before a new game starts playing the first song
-    start_timer: 1,
-    // duration of songs in seconds
-    song_duration: 3,
-    // time in seconds between each song
-    interval_timer: 5
-  },
-  
-  // socket
-  socket: {
-    port: 3030
-  },
+  module.exports = {
 
-  // database connection
-  db: {
-    host: 'localhost',
-    port: 27017,
-    database: 'songz'
+    // game parameters
+    game: {
+      // max number of players in a game
+      max_players: 15,
+      // number of songs played in a game
+      nb_songs: 15,
+      // time in seconds before a new game starts playing the first song
+      start_timer: 4,
+      // duration of songs in seconds
+      song_duration: 10,
+      // time in seconds between each song
+      interval_timer: 5
+    },
+    
+    // socket
+    socket: {
+      port: 3030
+    },
+
+    // database connection
+    db: {
+      host: 'localhost',
+      port: 27017,
+      database: 'songz_simon'
+    }
+
+  }
+
+}
+// SIMON DEV CONFIG
+else{
+
+  module.exports = {
+
+    // game parameters
+    game: {
+      // max number of players in a game
+      max_players: 15,
+      // number of songs played in a game
+      nb_songs: 15,
+      // time in seconds before a new game starts playing the first song
+      start_timer: 1,
+      // duration of songs in seconds
+      song_duration: 3,
+      // time in seconds between each song
+      interval_timer: 5
+    },
+    
+    // socket
+    socket: {
+      port: 3030
+    },
+
+    // database connection
+    db: {
+      host: 'localhost',
+      port: 27017,
+      database: 'songz'
+    }
+
   }
 
 }
